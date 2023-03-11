91ff9614036fd09867b03368dee25110b56a20f5
postmodernist1488 <elitic.pantheism@gmail.com>
2023-03-11 21:02:29 +0300

    convert to using shelve.Shelf

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
+'''
+TODO:
+1. Database
+2. Farm
+'''
 import aiogram
-from aiogram.types import Message
-from typing import Final, List, Set, Tuple, Dict
+from aiogram.types import Message, User
+from typing import Final, List, Set, Tuple
 import credentials
 import asyncio
 import signal
 import sys
-import os
 from datetime import datetime, timedelta
-import shutil
 from swearing import generate_swearline
 import linecache
 import random
 
-################################################################################
+import shelve
 
-DATABASE_DIR: Final[str] = 'database.d'
+##################### Globals ##################################################
+
 HELP_MESSAGE: Final[str] = '''Этот бот позволяет вам встать на путь sigma male grindset и гриндить каждый день.
 
 Команды:
@@ -47,11 +51,12 @@ dp = aiogram.Dispatcher(bot)
 
 RANKS: Final[List[str]] = ['пикочад', 'наночад', 'микрочад', 'чад', 'килочад', 'мегачад', 'терачад', 'экзачад',
         'зетачад', 'йотачад', 'богочад', 'дальше просто некуда']
-RANK_MARGINS: Final[Set[int]] = {1, 3, 5, 10, 20, 30, 50, 80, 100, 150}
+RANK_MARGINS_SET: Final[Set[int]] = {1, 3, 5, 10, 20, 30, 50, 80, 100, 150}
 
-GRINDCHECK_TIME: Final[Tuple[int, int]] = (20, 52)
+GRINDCHECK_TIME: Final[Tuple[int, int]] = (19, 20)
 
-database: Dict[int, int] = {}
+DATABASE_FILENAME: Final[str] = 'database.db'
+database: shelve.Shelf[int] = shelve.open(DATABASE_FILENAME)
 
 ################################################################################
 
@@ -68,9 +73,8 @@ async def send_welcome(message: Message):
 
 @dp.message_handler(commands=['grind'])
 async def grind(message: Message):
-    if message.from_user.id not in database:
-        database[message.from_user.id] = 0
-        save_user_to_file(message.from_user.id)
+    if str(message.from_user.id) not in database:
+        database[str(message.from_user.id)] = 0
         print(f'user {message.from_user.username} added to database')
         await bot.send_message(message.chat.id, "Вы записаны на гринд!")
     else:
@@ -79,7 +83,7 @@ async def grind(message: Message):
 
 @dp.message_handler(commands=['progress'])
 async def check_progress(message: Message):
-    if message.from_user.id in database:
+    if str(message.from_user.id) in database:
         await bot.send_message(message.chat.id, show_progress(message.from_user))
     else:
         await bot.send_message(message.chat.id, "Вы не записывались на грайнд. Чтобы стать сигмой, используйте /grind")
@@ -87,14 +91,13 @@ async def check_progress(message: Message):
 
 @dp.message_handler(commands=['lose'])
 async def lose(message: Message):
-    if message.from_user.id in database:
+    if str(message.from_user.id) in database:
 
         markup = aiogram.types.InlineKeyboardMarkup()
         item1 = aiogram.types.InlineKeyboardButton('Да', callback_data=Calldata.LOSE_YES)
         item2 = aiogram.types.InlineKeyboardButton('Нет', callback_data=Calldata.LOSE_NO)
         markup.add(item1, item2)
         await bot.send_message(message.chat.id, 'Вы точно хотите отписаться от гринда? (Весь прогресс будет потерян)', reply_markup=markup)
-
     else:
         await bot.send_message(message.chat.id, "Вы не записывались на грайнд. Чтобы стать сигмой, используйте /grind")
 
@@ -167,42 +170,13 @@ async def random_stoic_quote(message: Message):
         print('ans: ' + quote + author)
         await message.answer(quote + author)
 
-
-def save_database(path):
-    print('Saving the database')
-
-    # backup
-    try:
-        if os.path.exists(path + '.bak'):
-            shutil.rmtree(path + '.bak')
-        shutil.copytree(path, path + '.bak')
-    except FileNotFoundError:
-        print(f'Error: {DATABASE_DIR} not found')
-
-    for key, value in database.items():
-        with open(path + '/' + str(key), 'w') as f:
-            print(value, file=f)
-
-    print('Successfully saved the database')
-
-
-def save_user_to_file(userid):
-    with open(DATABASE_DIR + '/' + str(userid), 'w') as f:
-        print(database[userid], file=f)
-
-
-def remove_user_from_file(userid):
-    os.remove(DATABASE_DIR + '/' + str(userid))
-
-
 def sigint_handler():
-    if input('Do you want to save the database (y/n)').startswith('y'):
-        save_database(DATABASE_DIR)
+    database.close()
     sys.exit(0)
 
 
-def show_progress(user):
-    days = database[user.id]
+def show_progress(user: User):
+    days = database[str(user.id)]
 
     if user.username:
         status = f"{user.username}: гриндишь на протяжении {days} дней!\n"
@@ -248,7 +222,7 @@ def show_progress(user):
     return status
 
 
-def generate_progress_bar(start, current, maxvalue):
+def generate_progress_bar(start: int, current: int, maxvalue: int) -> str:
     length = 20
     filled_count = (current - start) * length // (maxvalue - start)
     return str(start) + ' [' + '■' * filled_count + '□' * (length - filled_count) + '] ' + str(maxvalue)
@@ -260,34 +234,34 @@ async def grindcheck():
     item1 = aiogram.types.InlineKeyboardButton('Да', callback_data=Calldata.GRIND_CHECK_YES)
     item2 = aiogram.types.InlineKeyboardButton('Нет', callback_data=Calldata.GRIND_CHECK_NO)
     markup.add(item1, item2)
-    asyncio.gather(*(bot.send_message(entry, 'Гриндил ли ты сегодня?', reply_markup=markup) for entry in database))
+    await asyncio.gather(*(bot.send_message(entry, 'Гриндил ли ты сегодня?', reply_markup=markup) for entry in database))
 
 
 @dp.callback_query_handler()
 async def callback(call):
     if call.message is None:
+        # the message may be deleted
         return
     match call.data:
         case Calldata.GRIND_CHECK_YES:
             await bot.edit_message_text(call.message.text, call.message.chat.id, call.message.message_id)
             database[call.from_user.id] += 1
-            save_user_to_file(call.from_user.id)
             await bot.send_message(call.message.chat.id, 'Keep up the grind!')
 
             # проверка на достижение нового уровня
-            if database[call.from_user.id] in RANK_MARGINS:
+            if database[call.from_user.id] in RANK_MARGINS_SET:
                 await bot.send_message(call.message.chat.id, 'Поздравляю, ты достиг нового звания! Используй /progress, чтобы узнать больше')
         case Calldata.GRIND_CHECK_NO:
             await bot.edit_message_text(call.message.text, call.message.chat.id, call.message.message_id)
             ans = generate_swearline() 
             await bot.send_message(call.message.chat.id, ans)
         case Calldata.ADMIN_SAVE_DATABASE:
-            save_database(DATABASE_DIR)
+            database.sync()
             await bot.send_message(call.message.chat.id, 'База данных успешно сохранена')
         case Calldata.ADMIN_SHOW_DATABASE:
             users = []
             for userid, data in database.items():
-                user_info = (await bot.get_chat_member(userid, userid)).user
+                user_info = (await bot.get_chat_member(userid, int(userid))).user
                 if user_info.username:
                     users.append(f'@{user_info.username} ({userid}): {data}')
                 else:
@@ -306,7 +280,6 @@ async def callback(call):
         case Calldata.LOSE_YES:
             await call.message.edit_reply_markup()
             del database[call.from_user.id]
-            remove_user_from_file(call.from_user.id)
             print(f'user {call.from_user.username} removed from database')
             await bot.send_message(call.message.chat.id, "Вы ушли с пути сигма гриндсета! Чтобы не быть ничтожеством, запишитесь на грайнд с помощью /grind")
         case Calldata.LOSE_NO:
@@ -318,32 +291,23 @@ async def callback(call):
 async def grindcheck_loop():
     while True:
         # проверка гринда каждый день
-
         hour, minute = GRINDCHECK_TIME[0] - 3, GRINDCHECK_TIME[1]
         now = datetime.utcnow()
         to = now.replace(hour=hour, minute=minute)
         if now >= to:
             to += timedelta(days=1)
-
         seconds_to_wait = (to - now).total_seconds()
-
         print(f'waiting for {seconds_to_wait} seconds')
         await asyncio.sleep(seconds_to_wait)
         await grindcheck()
 
-async def main():
-    
-    # read the database
-    print('Loading the database...')
 
-    try:
-        for filename in os.listdir(DATABASE_DIR):
-            with open(DATABASE_DIR + '/' + filename) as f:
-                database[int(filename)] = int(f.read())
-        print('Database loaded successfully:')
-        print(database)
-    except FileNotFoundError:
-        print(f'Error: {DATABASE_DIR} not found')
+async def main():
+
+    print("Database: {")
+    for key, value in database.items():
+        print(f'{key}: {value}')
+    print('}')
 
     asyncio.get_running_loop().add_signal_handler(signal.SIGINT, sigint_handler)
     await asyncio.gather(dp.start_polling(), grindcheck_loop())
